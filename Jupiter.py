2007898b00de4e3be3976abaa8a51b93279fb043
GinHar <gihape20@gmail.com>
2025-01-24 10:02:52 +0100

    Update Jupiter.py

--- Jupiter.py
+++ Jupiter.py
@@ -3,9 +3,12 @@ from RK5_complex import RK5_c
 import time
 
 # Saves in an unique archive .npy
-def save_to_unique_npy_file(base_filename, data, counter):
-    filename = f"{base_filename}_{counter}.npy"
+def save_file(base_filename, data, counter):
+    folder = "Data"
+    os.makedirs(folder, exist_ok=True)  # Create the directory
+    filename = os.path.join(folder, f"{base_filename}_{counter}.npy")
     np.save(filename, data)
+
 
 #Takes from a 3D array one section and calculates the derivative
 def der_pos_vel(x,t,M):
@@ -30,8 +33,8 @@ def der_pos_vel(x,t,M):
 
 start_time=time.time()
 
-dt = 3600*24
-t_fin = 3600*24*365*20000
+dt = 10
+t_fin = 3600*24*365*5
 t0 = 0
 t = np.arange(t0,t_fin,dt)
 N = len(t)
@@ -43,10 +46,10 @@ M = np.array([1.89813E27, 8.94E22, 4.80E22, 1.482E23, 1.075E23], dtype=complex) 
 #Define the vector. The first index is the time. The second index are the positions and the velocities. The third is the body.
 x = np.empty((1,4,(len(M))), dtype=complex)
 x[0,:,0] = np.array([0,0,0,0]) #Jupiter
-x[0,:,1] = np.array([4.216E8,0,0,np.sqrt(G*M[1]/4.216E8)]) #Io
-x[0,:,2] = np.array([6.709E8,0,0,np.sqrt(G*M[2]/6.709E8)]) #Europe
-x[0,:,3] = np.array([1.07E9,0,0,np.sqrt(G*M[3]/1.07E9)]) #Ganymede
-x[0,:,4] = np.array([1.883E9,0,0,np.sqrt(G*M[4]/1.883E9)]) #Callisto
+x[0,:,1] = np.array([4.216E8,0,0,np.sqrt(G*M[0]/4.216E8)]) #Io
+x[0,:,2] = np.array([6.709E8,0,0,np.sqrt(G*M[0]/6.709E8)]) #Europe
+x[0,:,3] = np.array([1.07E9,0,0,np.sqrt(G*M[0]/1.07E9)]) #Ganymede
+x[0,:,4] = np.array([1.883E9,0,0,np.sqrt(G*M[0]/1.883E9)]) #Callisto
 
 
 
@@ -54,20 +57,20 @@ t = np.arange(t0,t_fin,dt)
 
 
 counter = 1
-print(N/10000)
+print("It is going to be saved " + np.floor(N/10000) + "files")
 for i in np.arange(1,N):
     x = np.concatenate((x,RK5_c(der_pos_vel,np.squeeze(x[-1:,:,:]),t[i],dt,M)[np.newaxis,:,:]),axis = 0)
 
     if x.shape[0] >= 10000:
-        print("Copy "+ str(counter))
+        print("File "+ str(counter) + "calculated")
         # Convert the array to a NumPy format.
         np_x = np.real(np.array(x[:-1,:,:]))  # All except the last one
         # Save with an unique name
-        save_to_unique_npy_file("Jup", (np_x).real.astype(np.float64), counter)
+        save_file("Jup", (np_x).real.astype(np.float64), counter)
         counter += 1  # Increase the counter
         # Hold only the last one element
         x = x[-1:,:,:]
 
 end_time = time.time()
 
-print((end_time-start_time))
+print("It has taken " + (end_time-start_time) + " seconds")
